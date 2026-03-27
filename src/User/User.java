bf2dc059fc1eb656d7ce3db3002e9f3d32cbc7a9
gokdenizinan <gokdeniz.inan@wur.nl>
2026-03-27 16:41:38 +0100

    finish user class

--- src/User/User.java
+++ src/User/User.java
@@ -1,10 +1,37 @@
 package User;
 
+import UserCalendar.UserCalendar;
+
 public class User {
+	
+	public User(String username, String password, UserCalendar myCalendar, boolean isAdmin) {
+		this.username = username;
+		this.password = password;
+		this.myCalendar = myCalendar;
+		this.isAdmin = isAdmin;
+	}
+
+	private String username;
+	private String password;
+	private boolean isAdmin;
+	private UserCalendar myCalendar;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public UserCalendar getCalendar() {
+		return myCalendar;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
