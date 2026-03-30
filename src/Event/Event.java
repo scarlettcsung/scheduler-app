859394a7e831fe0ba2d92c74d1e797ec01d5c4ce
scarlettcsung <85265936+scarlettcsung@users.noreply.github.com>
2026-03-30 16:13:50 +0200

    SS NS: Initialize Event package

--- src/Event/Event.java
+++ src/Event/Event.java
@@ -1,4 +1,46 @@
 package Event;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import User.User;
+
 public class Event {
+
+    private String eventName;
+    public Date eventDate;
+    private int eventDuration;
+    private String eventDescription;
+    private User organizer;
+    private Boolean isImported;
+    private List<Invite> invites;
+
+    public Event(String eventName, Date eventDate, int eventDuration, String eventDescription,
+                 User organizer, Boolean isImported,  List<Invite> invites) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventDuration = eventDuration;
+        this.eventDescription = eventDescription;
+        this.organizer = organizer;
+        this.isImported = isImported;
+        if (invites == null) {
+            this.invites = new ArrayList<>();
+        } else {
+            this.invites = invites;
+        }
+    }
+
+    public User getOrganizer() {
+        return organizer;
+    }
+    public  List<Invite> getInvites() {
+        return invites;
+    }
+    public void addInvite(Invite invite) {
+        this.invites.add(invite);
+    }
+    public void removeInvite(Invite invite) {
+        this.invites.removeIf(n -> n == invite);
+    }
+
 }
