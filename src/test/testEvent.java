4109d2a450143bb923a7b9b0b170be96c3eb1588
scarlettcsung <85265936+scarlettcsung@users.noreply.github.com>
2026-03-31 16:54:27 +0200

    NS SS build testEvent, setup and method testAvailableSlot

--- src/test/testEvent.java
+++ src/test/testEvent.java
@@ -1,6 +1,29 @@
 package test;
 
+import Event.Event;
+import User.User;
+import UserCalendar.UserCalendar;
 import junit.framework.TestCase;
 
-public class testEvent {
+import java.util.Date;
+
+public class testEvent extends TestCase {
+
+    private UserCalendar calendar;
+    private User example_organizer;
+    private Event event;
+    private Date example_date;
+
+    protected void setUp() {
+        calendar = new UserCalendar();
+        example_organizer = new User("Charles","123456",calendar,false);
+        example_date = new Date(2026,01,01);
+        event = new Event("testEvent",example_date,60,"testEvent",
+                example_organizer,false,null);
+    }
+
+    public void testAvailableSlot() {
+        assertEquals(event.getOrganizer(),example_organizer);
+    }
+
 }
