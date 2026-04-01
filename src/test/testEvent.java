195e44bc8e98c42aee5d2e1ad2b89516c05f54f3
scarlettcsung <85265936+scarlettcsung@users.noreply.github.com>
2026-04-01 16:10:48 +0200

    SN SS Added setters to Event, changed Date to DateTime

--- src/test/testEvent.java
+++ src/test/testEvent.java
@@ -1,26 +1,31 @@
 package test;
 
 import Event.Event;
+import Invite.Invite;
 import User.User;
 import UserCalendar.UserCalendar;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class testEvent extends TestCase {
 
     private User example_organizer;
     private Event event;
-    private Date example_date;
+    private LocalDateTime example_time;
+    private Invite invite;
+    private User example_invitee;
 
     protected void setUp() {
         UserCalendar calendar = new UserCalendar();
         example_organizer = new User("Charles","123456", calendar,false);
-        example_date = new Date(126, 0, 01);
+        example_time = LocalDateTime.of(2026, 1, 1, 11, 0);
         event = new Event("testEvent", example_date,60,"testEvent",
                 example_organizer,false,null);
+        example_invitee = new User("Joe","789012", calendar,false);
+        invite = new Invite(example_invitee,event);
     }
 
     public void testEventName() {
@@ -28,8 +33,8 @@ public class testEvent extends TestCase {
         assertNotSame("wrongEvent",event.getEventName()); //Test QC
     }
 
-    public void testEventDate() {
-        assertEquals(example_date,event.getEventDate());
+    public void testEventTime() {
+        assertEquals(example_time,event.getEventTime());
     }
 
     public void testOrganizer() {
@@ -41,5 +46,10 @@ public class testEvent extends TestCase {
         assertEquals(expected,event.getInvites());
     }
 
-    //Invite to be completed
+    public void testAddInvite() {
+        event.addInvite(invite);
+        List<Invite> example_invites = List.of(invite);
+        assertEquals(example_invites,event.getInvites());
+
+    }
 }
