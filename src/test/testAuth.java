de7d35f09ba11ba6bb9d9f23792ba97a28374397
Ali <ali.islamanwarabdelhafezali@wur.nl>
2026-04-01 14:51:41 +0200

    EO AA Authorisation Test and Authorisation Class modification

--- src/test/testAuth.java
+++ src/test/testAuth.java
@@ -1,7 +1,23 @@
 package test;
 
 import junit.framework.TestCase;
+import Authentication.Authentication;
+
 
 public class testAuth extends TestCase {
+	public Authentication auth;
+	
+	public void testlogin() {
+	 	assertFalse(auth.login("John","password"));}
+	public void testauthorisedUser() {
+		auth.login("John","Pork");
+		assertEquals(auth.getauthenticatedUser().getUsername(),"John");}
+	public void testlogout() {
+		auth.login("John","Pork");
+		auth.logout();
+		assertNull(auth.getauthenticatedUser());}
+		
+
+	
 
 }
