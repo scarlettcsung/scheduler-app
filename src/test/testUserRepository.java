c6dadf1a188b93aab218486b76adb8d87ed964c4
gokdenizinan <gokdeniz.inan@wur.nl>
2026-04-02 16:21:29 +0200

    EO GI: corrected delete tests for userRepository after implementing admin distinction

--- src/test/testUserRepository.java
+++ src/test/testUserRepository.java
@@ -31,25 +31,36 @@ public class testUserRepository extends TestCase {
         User found = repository.findUsername("nonExistent");
         assertNull(found);
     }
-
-    public void testDeleteUserSuccess() {
-        repository.saveUser(testUser);
-        
-        boolean deleted = repository.deleteUserData("testUser");
-        
-        assertTrue(deleted);
-        assertNull(repository.findUsername("testUser"));
-    }
-
-    public void testDeleteUserNotFound() {
-        boolean deleted = repository.deleteUserData("MissingUser");
-        assertFalse(deleted);
-    }
+    	// Delete Checks :EO GI
 
     public void testDeleteNullUsername() {
-        boolean deleted = repository.deleteUserData(null);
-        assertFalse(deleted);
+ 
+        int deleted = repository.deleteUserData("MissingUser", null);
+        assertEquals(1, deleted);
     }
+    
+    public void testDeleteExistingUserAsAdmin() {
+    	// We will change once we have a file to save the user list and a permanent admin
+    	// Now we will just go on with a dummy admin user.
+		User adminUser = new User("admin", "admin", null, true);
+		repository.saveUser(testUser);
+		int deleted = repository.deleteUserData("testUser", adminUser);
+		assertEquals(2, deleted);
+			}
+	
+	public void testDeleteExistingUserAsSelf() {
+		repository.saveUser(testUser);
+		int deleted = repository.deleteUserData("testUser", testUser);
+		assertEquals(3, deleted);
+			}
+	
+	public void testDeleteExistingUserAsOther() {
+		User otherUser = new User("otherUser", "pw123", null, false);
+		repository.saveUser(testUser);
+		int deleted = repository.deleteUserData("testUser", otherUser);
+		assertEquals(4, deleted);
+		
+	}
 
     protected void tearDown() throws Exception {
         repository = null;
