c98a4589998d09f7a45ee1fbe5e8f164699481bc
gokdenizinan <gokdeniz.inan@wur.nl>
2026-04-02 20:44:00 +0200

    EO GI: add login method because of the OOP private restrictions so that it can securely login in userService class

--- src/UserService/UserService.java
+++ src/UserService/UserService.java
@@ -2,12 +2,15 @@ package UserService;
 
 import User.User;
 import UserRepository.UserRepository;
+import Authentication.Authentication;
 
 public class UserService {
     private UserRepository userRepository;
+    private Authentication authentication;
 // changed 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.authentication = new Authentication(userRepository);
     }
     
 // changed 
@@ -22,14 +25,19 @@ public class UserService {
         return true;
 		}
     }
+ // login implemented EO GI: 2/4/2026
+    public boolean login(String username, String password) {
+		return authentication.login(username, password);
+	}
 
     public boolean deleteUser(String username) {
 		if (!userRepository.isExistingUser(username)) {
 			return false;
 		}
-		User currentUser = userRepository.findUsername(username);
+		// Finally, it differentiates the current user
+		User currentUser = authentication.getauthenticatedUser();
 		int deleteStatus = userRepository.deleteUserData(username, currentUser);
-		if (deleteStatus == 2 || deleteStatus == 3) {
+		if (deleteStatus == 2 || deleteStatus == 3) { // 2->admin deleted, 3->self deleted
 			return true;
 		} else {
 			return false;
