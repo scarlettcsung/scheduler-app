f18b00f92e89785c386d83b8ea70aee20673393c
Sem van den Nieuwendijk <semvdn@gmail.com>
2026-04-02 19:21:01 +0200

    SN SS: Fixed testAuth
    Add setUp() to create UserRepository and Authentication.
    Fix testauthorisedUser and testlogout to assert successful login first.

--- src/test/testAuth.java
+++ src/test/testAuth.java
@@ -8,18 +8,31 @@ import UserRepository.UserRepository;
 
 
 public class testAuth extends TestCase {
-	public Authentication auth;	
+	public Authentication auth;
+	private UserRepository repository;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		repository = new UserRepository();
+		repository.saveUser(new User("John", "Pork", null, false));
+		auth = new Authentication(repository);
+	}
+
 	public void testlogin() {
 	 	assertFalse(auth.login("John","password"));
 	 	assertTrue(auth.login("John","Pork"));
-	 	}
+	}
+
 	public void testauthorisedUser() {
-		auth.login("John","Pork");
-		assertEquals(auth.getauthenticatedUser().getUsername(),"John");}
+		assertTrue(auth.login("John","Pork"));
+		assertEquals("John", auth.getauthenticatedUser().getUsername());
+	}
+
 	public void testlogout() {
-		auth.login("John","Pork");
+		assertTrue(auth.login("John","Pork"));
 		auth.logout();
-		assertNull(auth.getauthenticatedUser());}
+		assertNull(auth.getauthenticatedUser());
+	}
 		
 
 	
