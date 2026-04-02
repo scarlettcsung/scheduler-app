281e557020a96d8f7498ec82abb1b254ab689306
scarlettcsung <85265936+scarlettcsung@users.noreply.github.com>
2026-04-02 16:59:57 +0200

    SN SS CreateEvent GUI formatting

---
+++ src/GUI/CreateEvent.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CreateEvent {
+    private JPanel panel1;
+    private JTextField nameTextField;
+    private JFormattedTextField durationMinutesFormattedTextField;
+    private JFormattedTextField descriptionFormattedTextField;
+    private JFormattedTextField earliestDateFormattedTextField;
+    private JFormattedTextField latestDateFormattedTextField;
+    private JFormattedTextField earliestTimeFormattedTextField;
+    private JFormattedTextField latestTimeFormattedTextField;
+    private JButton inviteButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
