d1e0a89c612c7d1fcb4e54c6e50e900e71b14c55
gokdenizinan <gokdeniz.inan@wur.nl>
2026-04-02 23:52:39 +0200

    EO GI: create template for the userPanel

---
+++ src/GUI/UserPanel.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class UserPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public UserPanel() {
+
+	}
+
+}
