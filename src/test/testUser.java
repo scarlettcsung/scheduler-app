592917d4db66b6a386190298d04958dddf237b93
Niels de Jong <niels1.dejong@wur.nl>
2026-04-10 17:30:27 +0200

    NJ NS update User Test to increase coverage added 3 tests one for delete user while user is null 2 for admin can delete and cant delet users

--- src/test/testUser.java
+++ src/test/testUser.java
@@ -18,23 +18,44 @@ public class testUser extends TestCase {
         adminUser = new AdminUser("admin", "admin", null);
     }
 
+    //test the get username
     public void testGetUsername() {
         assertEquals("testUser", normalUser.getUsername());
     }
 
+    //test getpassword
     public void testGetPassword() {
         assertEquals("testPassword", normalUser.getPassword());
     }
 
+    //test get calendar
     public void testGetCalendar() {
         assertEquals(myCalendar, normalUser.getCalendar());
     }
 
+    //test acces to admin being denied as user
     public void testNormalUserCannotAccessAdminPanel() {
         assertFalse(normalUser.canAccessAdminPanel());
     }
 
+    //test admin user can access admin panel
     public void testAdminUserCanAccessAdminPanel() {
         assertTrue(adminUser.canAccessAdminPanel());
+    }
+    
+    //test delete user while user is null
+    public void testCanDeleteUserNull() {
+    	assertFalse(normalUser.canDeleteUser(null));
+    }
+    
+    //test the admin can delete user
+    public void testAdminCanDeleteExistingUser() {
+        User targetUser = new User("targetUser", "password", null);
+        assertTrue(adminUser.canDeleteUser(targetUser));
+    }
+    
+    //test the admin cannot delete user
+    public void testAdminCannotDeleteNullUser() {
+        assertFalse(adminUser.canDeleteUser(null));
     }
 }
