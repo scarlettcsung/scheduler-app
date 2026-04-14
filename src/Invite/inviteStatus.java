54b3596474de55a0410738675a682dd5d4fc87f0
Niels <niels.vanschie@wur.nl>
2026-04-14 16:34:08 +0200

    NJ NS added java doc to the invire class

--- src/Invite/inviteStatus.java
+++ src/Invite/inviteStatus.java
@@ -1,5 +1,11 @@
 package Invite;
 
+/**
+ * Enumerates the lifecycle states of an {@link Invite}.
+ *
+ * @author SS NS
+ * @version IT1
+ */
 public enum inviteStatus {
     PENDING,
     ACCEPTED,
