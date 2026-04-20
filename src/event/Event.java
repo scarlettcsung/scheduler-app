50fb7404de0202c837ff6d38725067f522f3fdff
Ali <ali.islamanwarabdelhafezali@wur.nl>
2026-04-20 19:38:36 +0200

    AA SS Modified Event class getParticipants & hasExsting...
    Modified get participants such that it was a true "getter" rather than
    creating a field and clearing it, just creating a new object when
    called. Moved hasExistingInvite() to EventManager. This is as it is used
    as a helper function in this class, and is more appropriate to its use.

--- src/event/Event.java
+++ src/event/Event.java
@@ -5,11 +5,7 @@ import Invite.Invite;
 // Additional Packages
 import java.util.Objects;
 import java.util.UUID;
-
-import javax.swing.JLabel;
-
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,79 +25,85 @@ public abstract class Event {
     private final String eventID = UUID.randomUUID().toString();
     private String organizerUsername;
     private List<Invite> invites;
-    private List<String> participantUsernames;
-    
+ 
+
     protected boolean isImportedField; // Just for IO
 
     public Event(String eventName, int eventDuration, String eventDescription,
-                 String organizerUsername,  List<Invite> invites) {
+            String organizerUsername, List<Invite> invites) {
         this.eventName = eventName;
         this.eventDuration = eventDuration;
         this.eventDescription = eventDescription;
         this.organizerUsername = organizerUsername;
         this.invites = Objects.requireNonNullElseGet(invites, () -> new ArrayList<>());
-        this.participantUsernames = new ArrayList<>();
+      
     }
-    
-    public abstract boolean isImported(); 
+
+    public abstract boolean isImported();
 
     // Setter Methods
-    public void setEventName(String eventName) {this.eventName = eventName;}
-    public void setEventTime(LocalDateTime eventTime) {this.eventTime = eventTime;}
-    public void setEventDescription(String eventDescription) {this.eventDescription = eventDescription;}
-    public void setEventDuration(int eventDuration) {this.eventDuration = eventDuration;}
-    public void setOrganizer(String organizerUsername) {this.organizerUsername = organizerUsername;}
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setEventTime(LocalDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public void setEventDuration(int eventDuration) {
+        this.eventDuration = eventDuration;
+    }
+
+    public void setOrganizer(String organizerUsername) {
+        this.organizerUsername = organizerUsername;
+    }
 
     // Getter Methods
-    public String getEventName() {return eventName;}
-    public LocalDateTime getEventTime() {return eventTime;}
-    public String getOrganizer() {return organizerUsername;}
-    public String getEventDescription() {return eventDescription; }
-    public int getEventDuration() {return eventDuration; }
-    public String getEventID() {return eventID; }
+    public String getEventName() {
+        return eventName;
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public String getOrganizer() {
+        return organizerUsername;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public int getEventDuration() {
+        return eventDuration;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
 
     // Invites Methods
-    
-    public  List<Invite> getInvites() {
+
+    public List<Invite> getInvites() {
         return invites;
     }
-    
+
     /**
      * Gets list of participant usernames
      */
     public List<String> getParticipants() {
+    	List<String> participantUsernames = new ArrayList<>();
     	participantUsernames.clear();
-    	
-    	for (Invite invite:invites) {
-    		String participantUsername = invite.getRecipient();
-    		participantUsernames.add(participantUsername);
-    	}
-    	return participantUsernames;
-    }
-    
-    /**
-     * Checks if event already has invite
-     *
-     * @param username string of the username of invited user
-     */
-    public boolean hasExistingInvite(String username) {
-    	for (Invite existingInvite: invites) {
-            if (existingInvite.getRecipient().equals(username)) {
-                return true;
-            }
-    	}
-    	return false;
-    }
-    
-    public String getTimeString() {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm");
-		String stringDateTime = eventTime.format(formatter);
-		LocalDateTime endTime = eventTime.plusMinutes(eventDuration);
-		String endTimeStr = endTime.format(DateTimeFormatter.ofPattern("HH:mm"));
-		String stringDisplay = String.format("%s - %s", stringDateTime, endTimeStr);
-
-		return stringDisplay;
+        for (Invite invite : invites) {
+            String participantUsername = invite.getRecipient();
+            participantUsernames.add(participantUsername);
+        }
+        return participantUsernames;
     }
 
-    
 }
