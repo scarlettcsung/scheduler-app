91d0fda3ab71cb50022d172998a5cfcec956e571
Ali <ali.islamanwarabdelhafezali@wur.nl>
2026-04-26 15:30:02 +0200

    AA SS Uploading Contents of BaseDashboard Panel
    Yesterday it would appear that the contents of this file were not pushed
    despite being present

--- src/gui/BaseDashboardPanel.java
+++ src/gui/BaseDashboardPanel.java
@@ -1,5 +1,34 @@
 package gui;
 
-public abstract class BaseDashboardPanel {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import repository.EventRepository;
+import repository.UserRepository;
+import scheduler.Scheduler;
+import user.User;
+
+public abstract class BaseDashboardPanel extends JPanel {
+	protected UserRepository repository;
+	protected EventRepository eventRepository;
+	protected Scheduler scheduler;
+	protected User activeUser;
+   
+    public BaseDashboardPanel(UserRepository repository, User user, Scheduler scheduler, EventRepository eventRepository) {
+        this.repository = repository;
+        this.activeUser = user;
+        this.scheduler = scheduler;
+        this.eventRepository = eventRepository;
+    }
+
+    protected void refreshEvents() {
+		JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+		topFrame.setContentPane(new UserPanel(repository, activeUser, scheduler,eventRepository));
+		topFrame.revalidate();
+		topFrame.repaint(); 
+		}
+  
+    protected abstract BaseDashboardPanel createRefreshPanel();
 
 }
